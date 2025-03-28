d387452c1c2f19af454f63fde05316bc3931e2c6
Salsa <andrei.pencu@outlook.com>
2025-03-28 14:16:10 +0200

    ihateuni

--- src/main/java/com/keko/affixLogics/GlyphMarking.java
+++ src/main/java/com/keko/affixLogics/GlyphMarking.java
@@ -4,6 +4,8 @@ import com.keko.items.custom.HeavyMirror;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.player.Player;
 
 import java.awt.*;
 
@@ -16,11 +18,14 @@ public class GlyphMarking {
 
     public static void renderCoordinates(GuiGraphics guiGraphics, DeltaTracker deltaTracker, float mirrorOffset) {
         int xMid = guiGraphics.guiWidth() / 2;
-        int yMid = guiGraphics.guiHeight() / 2;
+        int yMid = guiGraphics.guiHeight() / 2 - 4;
 
-        int alphaFactor = (int)(mirrorOffset * 100);
-        System.out.println(alphaFactor);
-        if ( alphaFactor > 10)
-            guiGraphics.drawCenteredString(Minecraft.getInstance().font, new String("Pulancur"), xMid, yMid, new Color(255, 255, 255, Math.max(0, alphaFactor * 2)).getRGB());
+        LocalPlayer player = Minecraft.getInstance().player;
+
+        int alphaFactor = (int)(mirrorOffset * 100 * 2);
+        if ( alphaFactor > 10 && player != null) {
+            guiGraphics.drawCenteredString(Minecraft.getInstance().font, String.format("%.3f", player.getX()), xMid - alphaFactor / 5, yMid, new Color(255, 255, 255, alphaFactor).getRGB());
+            guiGraphics.drawCenteredString(Minecraft.getInstance().font, String.format("%.3f", player.getZ()), xMid + alphaFactor / 5, yMid, new Color(255, 255, 255, alphaFactor).getRGB());
+        }
     }
 }
